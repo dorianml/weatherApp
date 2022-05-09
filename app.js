88b036ae7805cf79da20ae092406ac5cd2ac22c1
dmjahed <mjahedlucas.dorian@gmail.com>
2022-05-09 16:46:17 +0200

    add_files_day1

---
+++ app.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //--------------------------------------------
+
+
+const location = document.getElementById("userLocation")
+const submitButton = document.getElementById("submit")
+const resultDisplay = document.getElementsByTagName('h2')[0]
+const resultLatLng = document.getElementsByTagName('h2')[1]
+const weatherDisplay = document.getElementsByTagName('h3')[0]
+const API_KEY_loc = "9d4b2106584d4236b68d77703e0ec133"
+const API_KEY_weather = "6c601f4c97c69803c3d0ea71c97c199e"
+    // creation de l'event du bouton et lancer les deux api
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(location.value)
+    resultDisplay.innerHTML = `You are from ${location.value}!`
+    const userLocation = location.value
+    // Lancement de l'API pour détérminer la lat et lng de la ville entrée dans l'Input du form
+    let URL = `https://api.opencagedata.com/geocode/v1/json?q=${userLocation}&key=${API_KEY_loc}&language=fr&pretty=1`
+    fetch(URL) 
+    .then(response => { 
+        if (response.status == 200) { 
+            return response.json()
+        }
+        else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
+    })
+    .then(data => {
+        console.log(data.results[0])
+    const cityWeatherLat = data.results[0].geometry.lat
+    const cityWeatherLng = data.results[0].geometry.lng
+    resultLatLng.innerHTML = `Latitude: ${cityWeatherLat} , Longitude: ${cityWeatherLng}`
+    // Lancement de l'API OpenWeather pour inclure les lat et lng et trouvé la météo
+    let URL_weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherLat}&lon=${cityWeatherLng}&exclude={part}&appid=${API_KEY_weather}`
+    fetch(URL_weather)
+    .then(response => { 
+        if (response.status == 200) { 
+            return response.json()
+        }
+        else console.log(`Erreur lorsqu'on a tenté de récupérer les data Weather`);
+    })
+    .then(cityWeather => {
+        if (cityWeather.current.clouds > 50) {
+            console.log('CLOUDS')
+            weatherDisplay.innerHTML = `A lot of clouds today`
+        }
+        else if (cityWeather.current.clouds < 50 && cityWeather.current.clouds >= 50 ) {
+            weatherDisplay.innerHTML = `It's cloudy!`
+            console.log('CLOUDY')
+        }
+        else if (cityWeather.current.weather[0].main == 'Rain') {
+            weatherDisplay.innerHTML = `Shit, it's raining, don't forget your umbrella!`
+            console.log(cityWeather.current.weather[0].main)
+        }
+        else if (cityWeather.current.weather[0].main == 'Clear') {
+            weatherDisplay.innerHTML = `The sky is Clear!`
+            console.log(cityWeather.current.weather[0].main)
+        }
+    })
+    })
+    .catch(err2=> console.log(err2))
+
+
+    
+});
+    //--------------------------------------------
+});
