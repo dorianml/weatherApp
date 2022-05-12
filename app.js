7e5a338cdf0422b10fe563fc21bcd103f8288835
dmjahed <mjahedlucas.dorian@gmail.com>
2022-05-12 14:15:22 +0200

    Add night mode perfect, Left Factorisation

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     //--------------------------------------------
 
-//TODO: 1:Gerer les fonctions
-//TODO: 2 gerer le cache
 const location = document.getElementById("userLocation")
 const submitButton = document.getElementById("submit")
 
 const API_KEY_loc = "9d4b2106584d4236b68d77703e0ec133"
 const API_KEY_weather = "6c601f4c97c69803c3d0ea71c97c199e"
-const dateDisplay = document.getElementById('date')
-// IMG Weather Icone Const
-
-
 const formDisplayResult = document.getElementById('formResult')
-//
 
-// creation de l'event du bouton et lancer les deux api
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
     console.log(location.value)
-    // affichage de la ville selectioné (optionnel)
-    // resultDisplay.innerHTML = `You are from ${location.value}!`
     const userLocation = location.value
-    // Lancement de l'API pour détérminer la lat et lng de la ville entrée dans l'Input du form
     let URL = `https://api.opencagedata.com/geocode/v1/json?q=${userLocation}&key=${API_KEY_loc}&language=fr&pretty=1`
     fetch(URL) 
     .then(response => { 
@@ -32,15 +21,12 @@ submitButton.addEventListener("click", (event) => {
         else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
     })
     .then(data => {
-        
-        // Récup des lat et long de l'API OpenCageData
         const cityWeatherLat = data.results[0].geometry.lat
         const cityWeatherLng = data.results[0].geometry.lng
-        // Affichage de la longitude et de la latitude (optionnel)
-        // resultLatLng.innerHTML = `Latitude: ${cityWeatherLat} , Longitude: ${cityWeatherLng}`
-        // Lancement de l'API OpenWeather pour inclure les lat et lng et trouvé la météo
             let URL_weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherLat}&lon=${cityWeatherLng}&exclude={part}&appid=${API_KEY_weather}`
             console.log(URL_weather)
+
+            
             fetch(URL_weather)
             .then(response => { 
                 if (response.status == 200) { 
@@ -49,69 +35,66 @@ submitButton.addEventListener("click", (event) => {
                 else console.log(`Erreur lorsqu'on a tenté de récupérer les data Weather`);
             })
             .then(cityWeather => { 
+
                 formDisplayResult.innerHTML = []
-                // Récuperation de la Date du jour
                 const currentDate = new Date();
                 const currentDay = currentDate.getDay()
                 const weekDisplay = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi",
                             "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
-                // Fonction "choisir le nombre de jours prochains à afficher"
                 const dayOption = document.getElementById("day-select")
                 nbOfDays = dayOption.value 
-                // boucle d'affichage de la météo des jours séléxctionnés
-                for (let i= 0; i < nbOfDays ; i++) {
 
+                for (let i= 0; i < nbOfDays ; i++) {
                     const dayValue = weekDisplay[currentDay + i]
                     console.log(dayValue)
-                    // const dayTextDisplay = document.createElement('p')
-                    // dayTextDisplay.innerHTML = dayValue
                     const dayDiv = document.createElement('div')
-                    // dayDiv.classList.add('createdDays')
                     dayDiv.innerHTML = dayValue
                     const iconeWeather = document.createElement('img')
+                    let uviCheck = false
 
                     if (cityWeather.current.uvi == 0) {
-                        var uviCheck = true
-                        //Nigth mode
+                        uviCheck = true
                         document.getElementById("body").style.transition = "all 1s";
                         document.querySelector("body").style.background = "#06145F";  
-                        // document.getElementsByTagName(p).style.color = "#FFFFFF";  
+                        document.querySelector("body").style.color = "#FFFFFF";  
                     } else {
                         document.getElementById("body").style.transition = "all 1s";
                         document.querySelector("body").style.background = "#3399FF"
+                        document.querySelector("body").style.color = "#000000";  
                     }
-                    
-                    if (cityWeather.daily[i].weather[0].id == 803 || cityWeather.daily[i].weather[0].id == 804) {
-                        console.log(cityWeather.daily[i].weather[0].id)
+
+                    let iconeDisplayId = cityWeather.daily[i].weather[0].id
+
+                    if (iconeDisplayId == 803 || iconeDisplayId == 804) {
+                        console.log(iconeDisplayId)
                         if (uviCheck) {
                             iconeWeather.src = "./img/clouds-white.png"
                         } else iconeWeather.src = "./img/clouds.svg"
 
                     }
-                    else if (cityWeather.daily[i].weather[0].id == 801 || cityWeather.daily[i].weather[0].id == 802) {
-                        console.log(cityWeather.daily[i].weather[0].id)
+                    else if (iconeDisplayId == 801 || iconeDisplayId == 802) {
+                        console.log(iconeDisplayId)
                         if (uviCheck) {
                             iconeWeather.src = "./img/cloudy_white.png"
                         } else iconeWeather.src = "./img/cloudy.svg"
                     }
-                    else if (cityWeather.daily[i].weather[0].id <= 622 && cityWeather.daily[i].weather[0].id >= 600) {
-                        console.log(cityWeather.daily[i].weather[0].id)
+                    else if (iconeDisplayId <= 622 && iconeDisplayId >= 600) {
+                        console.log(iconeDisplayId)
                         if (uviCheck) {
                             iconeWeather.src = "./img/snow-white.png"
                         } else iconeWeather.src = "./img/snow.svg"
                     }
-                    else if (cityWeather.daily[i].weather[0].id == 800) {
-                        console.log(cityWeather.daily[i].weather[0].id)
+                    else if (iconeDisplayId == 800) {
+                        console.log(iconeDisplayId)
                         if (uviCheck) {
                             iconeWeather.src = "./img/sun-white.png"
                         } else iconeWeather.src = "./img/sun.svg"
                     }   
                     else {
-                        console.log(cityWeather.daily[i].weather[0].id)
+                        console.log(iconeDisplayId)
                         if (uviCheck) {
                             iconeWeather.src = "./img/rain-white.png"
                         } else iconeWeather.src = "./img/rain.svg"
-                        
                     }
                     formDisplayResult.appendChild(dayDiv)
                     dayDiv.appendChild(iconeWeather)
--- appv2.js
+++ appv2.js
@@ -1,106 +1,109 @@
-function getWeatherFromApi() {
-    let URL_weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherLat}&lon=${cityWeatherLng}&exclude={part}&appid=${API_KEY_weather}`
-    console.log(URL_weather)
-    fetch(URL_weather)
+document.addEventListener("DOMContentLoaded", function() {
+    //--------------------------------------------
+
+const location = document.getElementById("userLocation")
+const submitButton = document.getElementById("submit")
+
+const API_KEY_loc = "9d4b2106584d4236b68d77703e0ec133"
+const API_KEY_weather = "6c601f4c97c69803c3d0ea71c97c199e"
+const formDisplayResult = document.getElementById('formResult')
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(location.value)
+    const userLocation = location.value
+    let URL = `https://api.opencagedata.com/geocode/v1/json?q=${userLocation}&key=${API_KEY_loc}&language=fr&pretty=1`
+    fetch(URL) 
     .then(response => { 
         if (response.status == 200) { 
             return response.json()
         }
-        else console.log(`Erreur lorsqu'on a tenté de récupérer les data Weather`);
+        else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
     })
-    .then(cityWeather => { 
-        formDisplayResult.innerHTML = []
-        // Récuperation de la Date du jour
-    const newDate = new Date();
-    const day = newDate.getDay()
-    const week = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi",
-                "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
-    // Fonction "choisir le nombre de jours prochains à afficher"
-    const dayOption = document.getElementById("day-select")
-    j = dayOption.value 
-    // boucle d'affichage de la météo des jours séléxctionnés
-    for (let i= 0; i < j; i++) {
-        const dayValue = week[day + i]
-        const dayDiv = document.createElement('div')
-        dayDiv.innerHTML = dayValue
-        const iconeWeather = document.createElement('img')
-     // Changer le background color du body selon la tranche horaire de l'utilisateur
-        if (cityWeather.current.uvi == 0) {
-            document.querySelector("body").style.backgroundColor = "#00002B"
-            document.querySelector("body").style.color = "#00255"
+    .then(data => {
+        const cityWeatherLat = data.results[0].geometry.lat
+        const cityWeatherLng = data.results[0].geometry.lng
+            let URL_weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherLat}&lon=${cityWeatherLng}&exclude={part}&appid=${API_KEY_weather}`
+            console.log(URL_weather)
 
-        }
-        if (cityWeather.daily[i].weather[0].id == 803 || cityWeather.daily[i].weather[0].id == 804) {
-            console.log(cityWeather.daily[i].weather[0].id)
-            iconeWeather.src = "./img/clouds.svg"
-        }
-        else if (cityWeather.daily[i].weather[0].id == 801 || cityWeather.daily[i].weather[0].id == 802) {
-            console.log(cityWeather.daily[i].weather[0].id)
-            iconeWeather.src = "./img/cloudy.svg"
-           
-        }
-        else if (cityWeather.daily[i].weather[0].id <= 622 && cityWeather.daily[i].weather[0].id >= 600) {
-            console.log(cityWeather.daily[i].weather[0].id)
-            iconeWeather.src = "./img/snow.svg"
-           
-        }
-        else if (cityWeather.daily[i].weather[0].id == 800) {
-            console.log(cityWeather.daily[i].weather[0].id)
-            iconeWeather.src = "./img/sun.svg"
-           
-        }   
-        else {
-            console.log(cityWeather.daily[i].weather[0].id)
-            iconeWeather.src = "./img/rain.svg"
-           
-        }
-        formDisplayResult.appendChild(dayDiv)
-        dayDiv.appendChild(iconeWeather)
-    }
-})
-.catch(err=> console.log(err))
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    //--------------------------------------------
-    const location = document.getElementById("userLocation")
-    const submitButton = document.getElementById("submit")
-    // const resultDisplay = document.getElementsByTagName('h2')[0]
-    // const resultLatLng = document.getElementsByTagName('h2')[1]
-    // const weatherDisplay = document.getElementsByTagName('h3')[0]
-    const API_KEY_loc = "9d4b2106584d4236b68d77703e0ec133"
-    const API_KEY_weather = "6c601f4c97c69803c3d0ea71c97c199e"
-    const dateDisplay = document.getElementById('date')
-    // IMG Weather Icone Const
-    const formDisplayResult = document.getElementById('formResult')
-    //
-    // creation de l'event du bouton et lancer les deux api
-    submitButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        console.log(location.value)
-        // affichage de la ville selectioné (optionnel)
-        // resultDisplay.innerHTML = `You are from ${location.value}!`
-        const userLocation = location.value
-        // Lancement de l'API pour détérminer la lat et lng de la ville entrée dans l'Input du form
-        let URL = `https://api.opencagedata.com/geocode/v1/json?q=${userLocation}&key=${API_KEY_loc}&language=fr&pretty=1`
-        fetch(URL) 
-        .then(response => { 
-            if (response.status == 200) { 
-                return response.json()
-            }
-            else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
-        })
-        .then(data => {
             
-            // Récup des lat et long de l'API OpenCageData
-            const cityWeatherLat = data.results[0].geometry.lat
-            const cityWeatherLng = data.results[0].geometry.lng
-            // Affichage de la longitude et de la latitude (optionnel)
-            // resultLatLng.innerHTML = `Latitude: ${cityWeatherLat} , Longitude: ${cityWeatherLng}`
-            // Lancement de l'API OpenWeather pour inclure les lat et lng et trouvé la météo
-            getWeatherFromApi() 
+            fetch(URL_weather)
+            .then(response => { 
+                if (response.status == 200) { 
+                    return response.json()
+                }
+                else console.log(`Erreur lorsqu'on a tenté de récupérer les data Weather`);
+            })
+            .then(cityWeather => { 
+
+                formDisplayResult.innerHTML = []
+                const currentDate = new Date();
+                const currentDay = currentDate.getDay()
+                const weekDisplay = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi",
+                            "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
+                const dayOption = document.getElementById("day-select")
+                nbOfDays = dayOption.value 
+
+                for (let i= 0; i < nbOfDays ; i++) {
+                    const dayValue = weekDisplay[currentDay + i]
+                    console.log(dayValue)
+                    const dayDiv = document.createElement('div')
+                    dayDiv.innerHTML = dayValue
+                    const iconeWeather = document.createElement('img')
+                    let uviCheck = false
+
+                    if (cityWeather.current.uvi == 0) {
+                        uviCheck = true
+                        document.getElementById("body").style.transition = "all 1s";
+                        document.querySelector("body").style.background = "#06145F";  
+                        document.querySelector("body").style.color = "#FFFFFF";  
+                    } else {
+                        document.getElementById("body").style.transition = "all 1s";
+                        document.querySelector("body").style.background = "#3399FF"
+                        document.querySelector("body").style.color = "#000000";  
+                    }
+
+                    let iconeDisplayId = cityWeather.daily[i].weather[0].id
+
+                    if (iconeDisplayId == 803 || iconeDisplayId == 804) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/clouds-white.png"
+                        } else iconeWeather.src = "./img/clouds.svg"
+
+                    }
+                    else if (iconeDisplayId == 801 || iconeDisplayId == 802) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/cloudy_white.png"
+                        } else iconeWeather.src = "./img/cloudy.svg"
+                    }
+                    else if (iconeDisplayId <= 622 && iconeDisplayId >= 600) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/snow-white.png"
+                        } else iconeWeather.src = "./img/snow.svg"
+                    }
+                    else if (iconeDisplayId == 800) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/sun-white.png"
+                        } else iconeWeather.src = "./img/sun.svg"
+                    }   
+                    else {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/rain-white.png"
+                        } else iconeWeather.src = "./img/rain.svg"
+                    }
+                    formDisplayResult.appendChild(dayDiv)
+                    dayDiv.appendChild(iconeWeather)
+                }
+            })
+            .catch(err=> console.log(err))
         })
         .catch(err=> console.log(err))
+
     });
     //--------------------------------------------
 });
